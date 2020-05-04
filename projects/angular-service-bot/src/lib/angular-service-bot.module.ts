3ace6d2c526a7409ed9d16e3e31e042a8900a7aa
mehdi rajani <mehdi.rajani@iomechs.com>
2020-05-04 17:53:18 +0500

    fix(service-bot-module): fix the service issue in service bot

--- projects/angular-service-bot/src/lib/angular-service-bot.module.ts
+++ projects/angular-service-bot/src/lib/angular-service-bot.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { AngularServiceBotComponent } from './angular-service-bot.component';
+import { AngularServiceBotService } from './angular-service-bot.service';
 
 
 
@@ -7,6 +8,7 @@ import { AngularServiceBotComponent } from './angular-service-bot.component';
   declarations: [AngularServiceBotComponent],
   imports: [
   ],
-  exports: [AngularServiceBotComponent]
+  exports: [AngularServiceBotComponent],
+  providers: [AngularServiceBotService]
 })
 export class AngularServiceBotModule { }
